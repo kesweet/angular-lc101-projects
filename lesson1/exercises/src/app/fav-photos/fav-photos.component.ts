02edec7f856cb79cea14f457714de123018365a4
Katherine Sweet <katherine.sweet@slu.edu>
2019-10-21 12:55:42 -0500

    Part 1 Completed

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FavPhotosComponent implements OnInit {
   photosTitle = 'Photo/Image Section Title Here';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://assets3.thrillist.com/v1/image/2754967/size/tmg-article_tall;jpeg_quality=20.jpg';
+  image3 = 'https://images2.minutemediacdn.com/image/upload/c_crop,h_1065,w_1900,x_0,y_112/f_auto,q_auto,w_1100/v1565050342/shape/mentalfloss/594059-heartsbonesrescue.jpg';
 
   constructor() { }
 
